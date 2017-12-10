08c2dcc8be7305347ace660d4c2e0d26870fd5b3
engy-eleraky <engymahmoud323@yahoo.com>
2017-12-10 22:36:25 +0200

    fix widget

--- app/src/main/java/com/example/android/bakingrecipes/widget/BakingWidgetProvider.java
+++ app/src/main/java/com/example/android/bakingrecipes/widget/BakingWidgetProvider.java
@@ -1,30 +1,13 @@
 package com.example.android.bakingrecipes.widget;
 
-import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.net.Uri;
-import android.preference.PreferenceManager;
-import android.util.Log;
-import android.view.View;
 import android.widget.RemoteViews;
-
-import com.example.android.bakingrecipes.DetailsActivity;
 import com.example.android.bakingrecipes.R;
-import com.example.android.bakingrecipes.RecipeListFragment;
-import com.example.android.bakingrecipes.models.IngredientItem;
-import com.example.android.bakingrecipes.models.RecipeItem;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
-
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by Noga on 12/5/2017.
@@ -34,18 +17,18 @@ public class BakingWidgetProvider extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        Intent intent = new Intent(context, BakingWidgetRemoteViewsService.class);
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_view);
+        views.setTextViewText(R.id.recipe_name,BakingWidgetRemoteViewsService.name);
+        appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.recipe_name);
 
         for(int i = 0; i < appWidgetIds.length; i ++) {
-            Intent intent = new Intent(context, BakingWidgetRemoteViewsService.class);
             intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
             intent.putExtra("Random", Math.random() * 1000);
             intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_view);
-            //views.setRemoteAdapter( R.id.recipe_name, intent);
             views.setRemoteAdapter( R.id.widget_list, intent);
             appWidgetManager.updateAppWidget(appWidgetIds[i], views);
             appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds[i], R.id.widget_list);
-            //appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds[i], R.id.recipe_name);
 
         }
         super.onUpdate(context, appWidgetManager, appWidgetIds);
--- app/src/main/java/com/example/android/bakingrecipes/widget/BakingWidgetRemoteViewsService.java
+++ app/src/main/java/com/example/android/bakingrecipes/widget/BakingWidgetRemoteViewsService.java
@@ -1,21 +1,16 @@
 package com.example.android.bakingrecipes.widget;
 
-import android.app.IntentService;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
-import android.support.annotation.Nullable;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
-
 import com.example.android.bakingrecipes.DetailsActivity;
 import com.example.android.bakingrecipes.R;
-import com.example.android.bakingrecipes.RecipeListFragment;
 import com.example.android.bakingrecipes.models.IngredientItem;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-
 import java.util.ArrayList;
 
 /**
@@ -23,6 +18,7 @@ import java.util.ArrayList;
  */
 
 public class BakingWidgetRemoteViewsService extends RemoteViewsService {
+    public static String name;
 
 
     @Override
@@ -33,7 +29,6 @@ public class BakingWidgetRemoteViewsService extends RemoteViewsService {
 
     class MyWidgetRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
         Context context;
-        //String name;
         String ingredientList;
         private ArrayList<IngredientItem> ingredientItems;
         public MyWidgetRemoteViewsFactory(Context context) {
@@ -48,7 +43,7 @@ public class BakingWidgetRemoteViewsService extends RemoteViewsService {
         @Override
         public void onDataSetChanged() {
             SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-            //name=preferences.getString(DetailsActivity.SHARED_PREF_RECIPE_NAME,"");
+            name=preferences.getString(DetailsActivity.SHARED_PREF_RECIPE_NAME,"");
             String json = preferences.getString(DetailsActivity.SHARED_PREF_RECIPE_INGREDIENTS, "");
             if (!json.equals("")) {
                 Gson gson = new Gson();
@@ -72,14 +67,14 @@ public class BakingWidgetRemoteViewsService extends RemoteViewsService {
         @Override
         public RemoteViews getViewAt(int position) {
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_view_item);
-            String ingredient=ingredientItems.get(position).getIngredient();
-            String measure=ingredientItems.get(position).getMeasure();
-            String quantity=ingredientItems.get(position).getQuantity();
-            ingredientList=quantity +"\t"+measure;
-            views.setTextViewText(R.id.ingredient_quantity,ingredientList);
-            views.setTextViewText(R.id.ingredient_name,ingredient);
-
+            String ingredient = ingredientItems.get(position).getIngredient();
+            String measure = ingredientItems.get(position).getMeasure();
+            String quantity = ingredientItems.get(position).getQuantity();
+            ingredientList = quantity + "\t" + measure;
+            views.setTextViewText(R.id.ingredient_quantity, ingredientList);
+            views.setTextViewText(R.id.ingredient_name, ingredient);
             return views;
+
         }
 
         @Override
